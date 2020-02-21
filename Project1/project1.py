e5f8d28936c108e78a428767f90bdbd4f8dc041f
NateShetler <nateshetler21@gmail.com>
2020-02-21 13:33:58 -0500

    add winningSequence and uninformed game

--- Project1/project1.py
+++ Project1/project1.py
@@ -2,11 +2,15 @@
 """
 Created on Fri Feb 14 09:09:00 2020
 
-@author: adm_nds39
+@author: Nathaniel Shetler
+UANet ID: nds39
+ID: 4015423
+Class: Artificial Intelligence & Heuristic Programming
 """
 
 from queue import PriorityQueue
 import copy
+import time
 
 "Constants for the Program"
 NUM_ROWS = 3
@@ -31,8 +35,41 @@ def writeList(writeList, file):
             file.write(str(j) + " ")
             
     file.write("\n")
-            
 
+"This function calculates the Hamming priority for the game"
+def getHamming(numMoves, currentState, goalState):
+        
+    "Used for keeping track of the Hamming priority"
+    hammingPriority = 0
+        
+    "Go through the initial and goal states to see how many are out of position"
+    for i in range(0, NUM_ROWS):
+        for j in range(0, NUM_COLUMNS):
+            if goalState[i][j] != 0:
+                if goalState[i][j] != currentState[i][j]:
+                    hammingPriority += 1
+                    
+    hammingPriority += numMoves
+    
+    return hammingPriority
+
+"Pre: This function will accept in a currentState and goalState"
+"Post: The function will determine how close the states are to each other (similar to hamming)"
+def closeToGoal(currentState, goalState):
+    
+    "Used for keeping track of the Hamming priority"
+    hammingPriority = 0
+        
+    "Go through the initial and goal states to see how many are out of position"
+    for i in range(0, NUM_ROWS):
+        for j in range(0, NUM_COLUMNS):
+            if goalState[i][j] != 0:
+                if goalState[i][j] != currentState[i][j]:
+                    hammingPriority += 1
+    
+    return hammingPriority
+    
+    
 "Pre: This function will accept the inital state and goal state. This will check if the 8 puzzle game is solvable or not"
 "Post: This function returns true if it is solvable and false otherwise"
 def isSolvable(initial, goal):
@@ -153,23 +190,6 @@ class Game:
     "Mutator function for previousState"
     def updatePreviousState(self, newState):
         self.previousState = copy.deepcopy(newState)
-        
-    "This function calculates the Hamming priority for the game"
-    def getHamming(self, numMoves, currentState):
-        
-        "Used for keeping track of the Hamming priority"
-        hammingPriority = 0
-        
-        "Go through the initial and goal states to see how many are out of position"
-        for i in range(0, NUM_ROWS):
-            for j in range(0, NUM_COLUMNS):
-                if self.goalState[i][j] != 0:
-                    if self.goalState[i][j] != currentState[i][j]:
-                        hammingPriority += 1
-                    
-        hammingPriority += numMoves
-        
-        return hammingPriority
     
     def howManyNeighbors(self):
         "Will return how many neighbor states there are"
@@ -323,10 +343,105 @@ class Game:
         stack.append(currentState)
         
         return stack
+    
+    def winningSequence(self, stack):
+        
+        "Booleans for the loop"
+        neighborFound = False
+            
+        "For the top state/item on the stack/list"
+        topItem = []
+        
+        "Stack/list for the winning sequence"
+        winningSequence = []
+        
+        "Put the goalState on the stack/list"
+        winningSequence.append(self.goalState)
+        
+        "Make currentState the goal state"
+        currentState = copy.deepcopy(self.goalState)
+        
+        "Loop for going through the stack/list until a neighbor is found or end of stack is reached"
+        while len(stack) > 0:
+            
+            neighborFound = False
+            
+            "Get the next item off the stack/list"
+            topItem = stack.pop()
+            
+            if closeToGoal(currentState, topItem) == 1:              
+                
+                "See if the currentState is a neighbor of the top value"
+                if currentState[0][0] == 0:
+                    if topItem[0][1] == 0:
+                        neighborFound = True
+                    elif topItem[1][1] == 0:
+                        neighborFound = True
+                elif currentState[0][1] == 0:
+                    if topItem[0][0] == 0:
+                        neighborFound = True
+                    elif topItem[0][2] == 0:
+                        neighborFound = True
+                    elif topItem[1][1] == 0:
+                        neighborFound = True
+                elif currentState[0][2] == 0:
+                    if topItem[0][1] == 0:
+                        neighborFound = True
+                    elif topItem[1][2] == 0:
+                        neighborFound = True
+                elif currentState[1][0] == 0:
+                    if topItem[0][0] == 0:
+                        neighborFound = True
+                    elif topItem[1][1] == 0:
+                        neighborFound = True
+                    elif topItem[2][0] == 0:
+                        neighborFound = True
+                elif currentState[1][1] == 0:
+                    if topItem[0][1] == 0:
+                        neighborFound = True
+                    elif topItem[1][0] == 0:
+                        neighborFound = True
+                    elif topItem[1][2] == 0:
+                        neighborFound = True
+                    elif topItem[2][1] == 0:
+                        neighborFound = True
+                elif currentState[1][2] == 0:
+                    if topItem[0][2] == 0:
+                        neighborFound = True
+                    elif topItem[1][1] == 0:
+                        neighborFound = True
+                    elif topItem[2][2] == 0:
+                        neighborFound = True
+                elif currentState[2][0] == 0:
+                    if topItem[1][0] == 0:
+                        neighborFound = True
+                    elif topItem[2][1] == 0:
+                        neighborFound = True
+                elif currentState[2][1] == 0:
+                    if topItem[1][1] == 0:
+                        neighborFound = True
+                    elif topItem[2][0] == 0:
+                        neighborFound = True
+                    elif topItem[2][2] == 0:
+                        neighborFound = True
+                else:
+                    if topItem[1][2] == 0:
+                        neighborFound = True
+                    elif topItem[2][1] == 0:
+                        neighborFound = True
+                        
+                "Check to see if a neighbor was found"
+                if neighborFound == True:
+                    winningSequence.append(topItem)
+                    
+                currentState = copy.deepcopy(topItem)
+        
+        return winningSequence
             
                     
 class Play:
     
+    "This will play the 8 puzzle game informed using the hamming priority function"
     def startGame(self):
         "Create game object"
         game = Game()
@@ -350,17 +465,14 @@ class Play:
         "Boolean that will be used to keep track if puzzle is solved"
         solved = False
         
+        "This will be used to time the function"
+        start = time.time()
+        
         "Priority Queue for the states"
         pq = PriorityQueue()
         
         "Push on intial state and neighbors"
-        pq.put((game.getHamming(numMoves, game.currentState), game.initialState))
-            
-        "Will keep track of how many enqueus"
-        numEnqueues = 1
-        
-        "Keep track of number of neighbors for previous state"
-        numPrevNeighbors = 0
+        pq.put((getHamming(numMoves, game.currentState, game.goalState), game.initialState))
         
         "To keep track of states"
         checked = []
@@ -371,6 +483,9 @@ class Play:
         "Append the initial state to the stack"
         stack.append(game.initialState)
         
+        "This stack/list will be used to keep track of the winning sequence"
+        winningSequence = []
+        
         "Play until goal is reached"
         while solved == False:
             
@@ -380,13 +495,14 @@ class Play:
             "If state is not in the checked list, add it"
             if currentStateList not in checked: 
                 checked.append(currentStateList)
-                "Add to the stack if necessary"
-                stack = game.isNeighbor(currentStateList, stack)
+                
+                if currentStateList != game.goalState:   
+                    "Add to the stack if necessary"
+                    "stack = game.isNeighbor(currentStateList, stack)"
+                    stack.append(currentStateList)
             
             "Make the previous state the current state"
             game.updatePreviousState(game.currentState)
-            
-            numPrevNeighbors = game.howManyNeighbors()
             
             "Update the currentState"
             game.updateCurrentState(currentStateList)
@@ -421,11 +537,17 @@ class Play:
                 "Subtract 1 from numMoves because it counted initial state as a move at the beginning"
                 numMoves -= 1
                 
-                print("It took " + str(numMoves) + " moves to solve the puzzle.")
+                print("It took ", str(numMoves), " moves to solve the puzzle.")
                 
-                print(numEnqueues)
+                winningSequence = game.winningSequence(checked)
                 
-                print(stack)
+                for i in range(0, len(winningSequence)):
+                    printList(winningSequence[i])
+                    print()
+                
+                "Output how long it took"
+                end = time.time()
+                print("\nIt took ", str(round(end - start, 2)), " to solve the puzzle informed.")
                 
             else:
                 
@@ -436,25 +558,133 @@ class Play:
                 neighborList = game.neighborStates()
                 numNeighbors = game.howManyNeighbors()
                 
-                """"
-                "Remove old neighbors"
-                for i in range(0, numPrevNeighbors):
-                    if pq.empty() == False:
-                        pq.get()
-                """
-                numEnqueues = 0 
+                "If the state is not the previous state and hasn't been checked before then add to pq"
+                for i in range(0, numNeighbors):
+                    if game.previousState != neighborList[i] and neighborList[i] not in checked: 
+                        pq.put((getHamming(numMoves, neighborList[i], game.goalState), neighborList[i]))
+    
+    "This will play the 8 puzzle game uninformed"
+    def startGameUninformed(self):
+        "Create game object"
+        game = Game()
+        
+        "This will be the file used to store the sequence"
+        file = open("8puzzlelog.txt", "w")
+        
+        "Get initial and goal states"
+        game.getInitial()
+        game.getGoal()
+        
+        "Set currentState to initial state"
+        game.updateCurrentState(game.initialState)
+        
+        "Set initial previous state to all 0's"
+        game.updatePreviousState([[0,0,0],[0,0,0],[0,0,0]])
+        
+        "For keeping track of how many moves"
+        numMoves = 0
+        
+        "Boolean that will be used to keep track if puzzle is solved"
+        solved = False
+        
+        "This will be used to time the function"
+        start = time.time()
+        
+        "Priority Queue for the states"
+        pq = PriorityQueue()
+        
+        "Push on intial state and neighbors"
+        pq.put((1, game.initialState))
+        
+        "To keep track of states"
+        checked = []
+        
+        "This list that will function as a stack will keep track of the correct wining sequence"
+        stack = []
+        
+        "Append the initial state to the stack"
+        stack.append(game.initialState)
+        
+        "This stack/list will be used to keep track of the winning sequence"
+        winningSequence = []
+        
+        "Play until goal is reached"
+        while solved == False:
+            
+            "Get the current list from the priority queue"
+            currentStateList = pq.get()[1]
+            
+            "If state is not in the checked list, add it"
+            if currentStateList not in checked: 
+                checked.append(currentStateList)
+                
+                if currentStateList != game.goalState:   
+                    "Add to the stack if necessary"
+                    "stack = game.isNeighbor(currentStateList, stack)"
+                    stack.append(currentStateList)
+            
+            "Make the previous state the current state"
+            game.updatePreviousState(game.currentState)
+            
+            "Update the currentState"
+            game.updateCurrentState(currentStateList)
+            
+            "Add one to the move counter"
+            numMoves += 1
+            
+            "If the currentState is the goal state"
+            if (game.currentState == game.goalState):
+                
+                "Set solved to true"
+                solved = True
+                
+                print("The puzzle has been sovled!\n")
+                
+                "Write to file the solved puzzle"
+                writeList(currentStateList, file)
+                
+                "Close write file"
+                file.close()
+                
+                "Open readfile"
+                readFile = open("8puzzlelog.txt", "r")
+                
+                "Output the sequence log"
+                print("The sequence that it was solved in is: ")
+                print(readFile.read())
+                
+                "Close read file"
+                readFile.close()
+                
+                "Subtract 1 from numMoves because it counted initial state as a move at the beginning"
+                numMoves -= 1
+                
+                print("It took ", str(numMoves), " moves to solve the puzzle.")
+                
+                winningSequence = game.winningSequence(checked)
+                
+                for i in range(0, len(winningSequence)):
+                    printList(winningSequence[i])
+                    print()
+                    
+                "Output how long it took"
+                end = time.time()
+                print("\nIt took ", str(round(end - start, 2)), " to solve the puzzle uninformed.")
+                
+            else:
+                
+                "Write to file"
+                writeList(currentStateList, file)
+                
+                "Update neighborList and put the new neighbors states on the queue"
+                neighborList = game.neighborStates()
+                numNeighbors = game.howManyNeighbors()
                 
                 "If the state is not the previous state and hasn't been checked before then add to pq"
                 for i in range(0, numNeighbors):
                     if game.previousState != neighborList[i] and neighborList[i] not in checked: 
-                        pq.put((game.getHamming(numMoves, neighborList[i]), neighborList[i]))
-
+                        pq.put((i, neighborList[i]))
                         
-                        numEnqueues += 1
-                
-                if numEnqueues == 0:
-                    print("Reached a dead end")
-                    file.write("Reached a dead end")
                     
 def n_puzzle():
     
