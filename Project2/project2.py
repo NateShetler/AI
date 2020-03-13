01217f415997a020c9a3b63d3cc43318d33c50d6
NateShetler <nateshetler21@gmail.com>
2020-03-13 12:13:14 -0400

    add project2

---
+++ Project2/project2.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 09:59:46 2020
+
+@author: adm_nds39
+"""
+import pandas as pd
+
+def py_nb():
+    
+    "Boolean for the menu loop"
+    endMenu = False
+    
+    while endMenu == False:
+        
+        "Print the menu out"
+        print("1. Learn a Naïve Bayesian classifier from categorical data.")
+        print("2. Save a model.")
+        print("3. Load a model and test its accuracy.")
+        print("4. Apply a naïve Bayesian classifier to new cases interactively. The submeny for this item includes:")
+        print("\t4.1 Enter a new case interactively.")
+        print("\t4.2 Quit.")
+        print("5. Quit.")
+        
+        "Get the user's choice"
+        menuChoice = input()
+        
+        "Do something based on the user input"
+        
+        if menuChoice == "5":
+            endMenu = True
+        elif menuChoice == "1":
+            fileName = input("Please enter the name of the csv file you would like to enter: ")
+            
+            "Get the csv data"
+            csvData = getCSV(fileName)
+            
+
+"Pre: This function will accept in the name of a csv file"
+"Post: This function will get the contents of the csv file and" 
+"store them in a dataFrame. It will also return that dataFrame"           
+def getCSV(file):
+    
+    "Create the dataFrame from the csv file"
+    csvDataFrame = pd.read_csv(file)
+    
+    "Print out the contents of the csv file"
+    print(csvDataFrame)
+        
+    "For formatting purposes"
+    print()
+    
+    return csvDataFrame
